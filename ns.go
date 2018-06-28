607bd530f56f7b8ef08e3f9f4f17abf0f0c4c5f2
Scott Nichols <nicholss@google.com>
2018-06-28 16:50:37 -0700

    Need fallthrough.

--- ns.go
+++ ns.go
@@ -8,10 +8,13 @@ import (
 func main() {
 	switch os.Args[1] {
 	case "checkout":
+		fallthrough
 	case "branch":
+		fallthrough
 	case "co":
 		cmd.Checkout(os.Args[2:])
 	case "open":
+		fallthrough
 	case "o":
 		cmd.Open(os.Args[2:])
 	case "status":
