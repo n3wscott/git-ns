e06dd2d6d590c1d898d0bd7a8a140a015c27bd7e
Scott Nichols <nicholss@google.com>
2018-06-28 16:42:43 -0700

    Better.

--- ns.go
+++ ns.go
@@ -7,13 +7,17 @@ import (
 
 func main() {
 	switch os.Args[1] {
+	case "checkout":
 	case "branch":
-		cmd.Branch(os.Args[2:])
+	case "co":
+		cmd.Checkout(os.Args[2:])
 	case "open":
+	case "o":
 		cmd.Open(os.Args[2:])
 	case "status":
 		cmd.Status(os.Args[2:])
 	case "sync":
+	case "s":
 		cmd.Sync(os.Args[2:])
 	default:
 		cmd.Help(os.Args[2:])
